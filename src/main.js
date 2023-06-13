73d11218b8f28e630099a9cf253db00fab3de4d1
Nicolás <117678560+mkdir-nicolas@users.noreply.github.com>
2023-06-12 22:11:11 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,32 +1,51 @@
 const ionIcon = document.querySelector("ion-icon");
-let state = {
+const dataAreaShare = document.querySelector('[data-type="share"]');
+const state = {
   button: 0,
 };
 
 const shareUI = () => {
   state.button = 1;
   return `
-        <aside class="footer-share fadeEffect" id="shareElement">
+        <aside class="footer-share fadeEffect" data-type="share" id="shareElement">
             <h2>Share</h2>
-            <div class="footer-share-icons">
-                <a href="#" class="facebook" aria-label="Facebook"></a>
+            <div class="footer-share-icons" data-type="share">
+                <a href="#"  class="facebook" aria-label="Facebook"></a>
                 <a href="#" class="twitter" aria-label="Twitter"></a>
                 <a href="#" class="pinterest" aria-label="Pinterest"></a>
             </div>
-            <span class="triangle-down"></span>
+            <span class="triangle-down" data-type="share"></span>
         </aside>
          `;
 };
 
-btnShare.addEventListener("click", (e) => {
-  if (state.button == 0) {
-    ionIcon.style.filter = "var(--secondaryFilter)";
-    btnShare.style.backgroundColor = "var(--desaturated-dark-blue)";
-    card.insertAdjacentHTML("afterbegin", shareUI());
-  } else {
-    btnShare.style.backgroundColor = "var(--light-grayish-blue)";
-    ionIcon.style.filter = "var(--primaryFilter)";
-    shareElement.remove();
-    state.button = 0;
-  }
+const addShare = function () {
+  btnShare.style.backgroundColor = "var(--desaturated-dark-blue)";
+  ionIcon.style.filter = "var(--secondaryFilter)";
+  card.insertAdjacentHTML("afterbegin", shareUI());
+  state.button = 1;
+};
+
+const removeShare = function () {
+  if (state.button == 0) return;
+  btnShare.style.backgroundColor = "var(--light-grayish-blue)";
+  ionIcon.style.filter = "var(--primaryFilter)";
+  shareElement.remove();
+  state.button = 0;
+};
+
+const render = function () {
+  state.button == 0 ? addShare() : removeShare();
+};
+
+const isSameElement = (a, b) => {
+  return a == b;
+};
+
+btnShare.addEventListener("click", (e) => render());
+
+document.addEventListener("click", (e) => {
+  const dataSetTarget = dataAreaShare.dataset.type;
+  let dataSet = e.explicitOriginalTarget.dataset.type;
+  if (!isSameElement(dataSet, dataSetTarget)) removeShare();
 });
